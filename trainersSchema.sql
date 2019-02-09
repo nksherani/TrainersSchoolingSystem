eff34bc71ab0fd73c8dbb0c5ed6331113a05fce8
naveed <nksherani@outlook.com>
2019-02-09 13:22:57 +0500

    no message

--- trainersSchema.sql
+++ trainersSchema.sql
@@ -1,7 +1,7 @@
 USE [master]
 GO
 
-/****** Object:  Database [Trainers]    Script Date: 2/9/2019 12:49:22 PM ******/
+/****** Object:  Database [Trainers]    Script Date: 2/9/2019 1:22:45 PM ******/
 CREATE DATABASE [Trainers]
  CONTAINMENT = NONE
  ON  PRIMARY 
