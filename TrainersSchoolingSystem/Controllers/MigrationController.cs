2d180cc1b05823d2bd74005f2c8399e6128fc1b9
naveed <nksherani@outlook.com>
2019-03-13 20:37:01 +0500

    General Staff management started Migration script started

---
+++ TrainersSchoolingSystem/Controllers/MigrationController.cs
@@ -0,0 +1,114 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrainersSchoolingSystem.Controllers
+{
+    public class MigrationController : Controller
+    {
+        // GET: Migration
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+
+            if (file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(path);
+            }
+            ViewBag.Message = "Data Uploaded Successfully";
+            return RedirectToAction("Index");
+        }
+        private bool ProcessStudentsData(string path)
+        {
+            var package = new ExcelPackage(new FileInfo(path));
+
+            ExcelWorksheet workSheet = package.Workbook.Worksheets[1];
+
+            for (int i = workSheet.Dimension.Start.Row;
+                     i <= workSheet.Dimension.End.Row;
+                     i++)
+            {
+                object cellValue = workSheet.Cells[i, 0].Value;
+            }
+            
+            return true;
+        }
+        // GET: Migration/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Migration/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Migration/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Migration/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Migration/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Migration/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Migration/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TrainersSchoolingSystem/Models/DTOs/StaffPaySlipModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrainersSchoolingSystem.Models.DTOs
+{
+    public class StaffPaySlipModel
+    {
+        public int ChallanNo { get; set; }
+        public string Month { get; set; }
+        public DateTime? IssueDate { get; set; }
+        public int StaffId { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public DateTime? JoiningDate { get; set; }
+        public decimal BasicPay { get; set; }
+        public decimal Bonus { get; set; }
+        public decimal PF { get; set; }
+        public decimal EOBI { get; set; }
+        public decimal LoanDeduction { get; set; }
+        public decimal GrossPay { get; set; }
+        public int UnpaidLeaves { get; set; }
+        public decimal AmountDeducted { get; set; }
+        public decimal NetPay { get; set; }
+    }
+}
