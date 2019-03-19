7e3cd0515cd339bb1479d7be23130c30482291ef
naveed <nksherani@outlook.com>
2019-03-19 23:59:29 +0500

    no message

---
+++ TrekBikes/TrekBikes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrekBikes.MvcApplication" Language="C#" %>
---
+++ TrekBikes/TrekBikes/Models/SurveyObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrekBikes.Models
+{
+
+    [Serializable]
+    public class SurveyObject
+    {
+        public List<string> bikes = new List<string>();
+
+    }
+}
